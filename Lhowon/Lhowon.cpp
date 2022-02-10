54f570e06b68af86d60af9f7463998787a0d1cc7
Josiah Oberholtzer <josiah.oberholtzer@gmail.com>
2022-02-10 13:37:49 -0500

    Sketching

--- Lhowon/Lhowon.cpp
+++ Lhowon/Lhowon.cpp
@@ -8,11 +8,15 @@ using namespace daisysp;
 
 DaisyVersio hw;
 
-Panner panner_l;
-Panner panner_r;
+Panner panner_a;
+Panner panner_b;
+CrossFade xfader;
 
-CrossFade xfade_l;
-CrossFade xfade_r;
+static float buffer[2];
+static float panned_a[2];
+static float panned_b[2];
+static float feedback[2];
+static float feedback_level;
 
 void callback(
     AudioHandle::InterleavingInputBuffer  in,
@@ -20,19 +24,30 @@ void callback(
     size_t                                size
 ) {
     for (size_t i = 0; i < size; i += 2) {
-        out[i] = 0.0f;
-        out[i + 1] = 0.0f;
+        buffer[0] = SoftClip(in[i] + (feedback[0] * feedback_level));
+        buffer[1] = SoftClip(in[i + 1] + (feedback[1] * feedback_level));
+
+        panner_a.Process(buffer[0], panned_a);
+        panner_b.Process(buffer[1], panned_b);
+
+        buffer[0] = xfader.Process(panned_a[0], panned_b[0]);
+        buffer[1] = xfader.Process(panned_a[1], panned_b[1]);
+
+        feedback[0] = buffer[0];
+        feedback[1] = buffer[1];
+
+        out[i] = buffer[0];
+        out[i + 1] = buffer[1];
     }
 }
 
 int main(void) {
     hw.Init();
 
-    panner_l.Init();
-    panner_r.Init();
-
-    xfade_l.Init(CROSSFADE_CPOW);
-    xfade_r.Init(CROSSFADE_CPOW);
+    feedback_level = 0.f;
+    panner_a.Init();
+    panner_b.Init();
+    xfader.Init(CROSSFADE_CPOW);
 
     hw.StartAudio(callback);
     hw.StartAdc();
@@ -42,10 +57,9 @@ int main(void) {
         hw.UpdateExample();
         hw.UpdateLeds();
 
-        panner_l.SetPos(hw.GetKnobValue(DaisyVersio::KNOB_1));
-        panner_r.SetPos(hw.GetKnobValue(DaisyVersio::KNOB_5));
-
-        xfade_l.SetPos(hw.GetKnobValue(DaisyVersio::KNOB_3));
-        xfade_r.SetPos(hw.GetKnobValue(DaisyVersio::KNOB_3));
+        panner_a.SetPos(hw.GetKnobValue(DaisyVersio::KNOB_0));
+        xfader.SetPos(hw.GetKnobValue(DaisyVersio::KNOB_2));
+        panner_b.SetPos(hw.GetKnobValue(DaisyVersio::KNOB_4));
+        feedback_level = hw.GetKnobValue(DaisyVersio::KNOB_6);
     }
 }
--- Lhowon/Panner.h
+++ Lhowon/Panner.h
@@ -9,9 +9,9 @@ public:
 
     inline void Init() { pos_ = 0.5f; }
 
-    void Process(float &in, float* out) {
-        out[0] = sinf((1.0f - pos_) * HALFPI_F);
-        out[1] = sinf(pos_ * HALFPI_F);
+    void Process(float in, float* out) {
+        out[0] = in * sinf((1.0f - pos_) * HALFPI_F);
+        out[1] = in * sinf(pos_ * HALFPI_F);
     }
 
     inline void SetPos(float pos) { pos_ = pos; }
